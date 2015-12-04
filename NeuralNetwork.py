41670adafdbb02622703a3bee6dafd16f62ca30d
AhmedElassuty <ahmed.elassuty214@gmail.com>
2015-12-04 21:31:11 +0200

    ID_RBF construction

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -14,31 +14,34 @@ class ID_RBF:
     self.ni = inputNodes
     self.no = outputNodes
 
-    # initialize nodes width
+    # Initializing nodes width
     # width vector of the hidden nodes
     self.wh = np.matrix([])
 
     # output vector of the hidden nodes
     self.ao = np.matrix([])
 
-    # initialize weights
-    # centers intially empty since
-    # initially no hidden nodes
-    self.centers = []
+    # Initializing weights
+    # Centers are empty since
+    #   initially no hidden nodes
+    # col: represents the corresponding RBF unit
+    # row: represents the center value 
+    #   with respect to the corresponding input node
+    #  *bias: no bias associated with RBF units
+    self.centers = np.matrix([])
 
     # Directly connect the output
     # matrix defines the connections from the input layer to
     # the output layer
-    # col: specifies the corresponding output unit
-    # row: specifies the corresponding weight
-    #   bias: is the first row of the matrix
+    # col: represents the corresponding output unit
+    # row: represents the weight value 
+    #   with respect to the connected node
+    #   *bias: is the first row of the matrix
     self.wo = np.matrix([[1.0]*self.ao]*(self.ni+1))
 
   def train(self, patterns):
     if self.ni is not patterns.shape[1]:
       assert "specified patterns have different dimensions as input"
-
-
     return "trained"
 
 
